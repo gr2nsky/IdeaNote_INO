dfafd5165feb6f358d333fd669dbcbfa2b4e2827
gr2nsky <dbswovlf2009@naver.com>
2021-07-17 17:18:22 +0900

    Activity - layout item resource 연결
    추가적으로 mian - addIdeaDialog 출력까지
    
    기능은 비어있음

--- app/src/main/java/com/example/ideanote_ino/Activity/MainActivity.java
+++ app/src/main/java/com/example/ideanote_ino/Activity/MainActivity.java
@@ -4,18 +4,45 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.ideanote_ino.Dialog.InputIdeaDialog;
 import com.example.ideanote_ino.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView iv_main_search;
+    ImageView iv_main_see_all;
+    TextView tv_main_idea;
+    TextView tv_main_idea_date;
+    ImageView iv_main_show_before_idea;
+    ImageView iv_main_add_idea;
+    ImageView iv_main_show_next_idea;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(MainActivity.this, LogoActivity.class);
-        startActivity(intent);
-        finish();
+        iv_main_search = findViewById(R.id.iv_main_search);
+        iv_main_see_all = findViewById(R.id.iv_main_see_all);
+        tv_main_idea = findViewById(R.id.tv_main_idea);
+        tv_main_idea_date = findViewById(R.id.tv_main_idea_date);
+        iv_main_show_before_idea = findViewById(R.id.iv_main_show_before_idea);
+        iv_main_add_idea = findViewById(R.id.iv_main_add_idea);
+        iv_main_show_next_idea = findViewById(R.id.iv_main_show_next_idea);
+
+        iv_main_add_idea.setOnClickListener(clickedAddIdea);
     }
+
+    View.OnClickListener clickedAddIdea = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            InputIdeaDialog.show(MainActivity.this);
+        }
+    };
+
+
 }
--- app/src/main/java/com/example/ideanote_ino/Dialog/InputIdeaDialog.java
+++ app/src/main/java/com/example/ideanote_ino/Dialog/InputIdeaDialog.java
@@ -3,14 +3,22 @@ package com.example.ideanote_ino.Dialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
 import com.example.ideanote_ino.R;
 
 public class InputIdeaDialog extends Dialog {
-
+    String TAG = "#InputIdeaDialog";
     private Context con;
+
+    EditText et_input_idea_dialog_idea;
+    TextView tv_input_idea_dialog_negative;
+    TextView tv_input_idea_dialog_positive;
 
     public InputIdeaDialog(@NonNull Context context) {
         super(context);
@@ -21,7 +29,30 @@ public class InputIdeaDialog extends Dialog {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dialog_main_input_idea);
+
+        et_input_idea_dialog_idea = findViewById(R.id.et_input_idea_dialog_idea);
+        tv_input_idea_dialog_negative = findViewById(R.id.tv_input_idea_dialog_negative);
+        tv_input_idea_dialog_positive = findViewById(R.id.tv_input_idea_dialog_positive);
     }
 
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (v == tv_input_idea_dialog_negative){
+                Log.v(TAG, "touched tv_input_idea_dialog_negative");
+                dismiss();
+                return;
+            }
+            //SQLite에 입력한 아이디어를 저장하는 부분
+            Log.v(TAG, "touched tv_input_idea_dialog_positive");
+        }
+    };
 
+
+    public static void show(Context con){
+        InputIdeaDialog dialog = new InputIdeaDialog(con);
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.setCancelable(true);
+        dialog.show();
+    }
 }
