4ba31935a43485472c95e6a57efa3d8722eac6bc
gr2nsky <dbswovlf2009@naver.com>
2021-07-21 23:25:40 +0900

    메인 엑티비티 수정
    setRandomIdea 구동시 배열이 비어있다면
    setOnClickListener(null)로 클릭리스너를 제거
    아닐경우 원래 리스너 부착
    추가적으로 onresume삭제하고 온크리에이트로 옮김

--- app/src/main/java/com/example/ideanote_ino/Activity/MainActivity.java
+++ app/src/main/java/com/example/ideanote_ino/Activity/MainActivity.java
@@ -44,8 +44,6 @@ public class MainActivity extends AppCompatActivity {
 
         iv_main_add_idea.setOnClickListener(clickedAddIdea);
         iv_main_refrash_idea.setOnClickListener(showRandomIdea);
-        tv_main_idea.setOnClickListener(ideaClickEvent);
-        tv_main_idea_date.setOnClickListener(ideaClickEvent);
         iv_main_see_all.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -53,27 +51,28 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-    }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
         if (!queryForMain.selectAllList()){
             Toast.makeText(this, "아이디어 목록을 불러오는데 실패했습니다. 앱을 다시 시작해주세요.", Toast.LENGTH_SHORT).show();
         }
         setRandomIdea();
-    }
-
+    } //onCreate
+    
     public void setRandomIdea(){
         IdeaDatas ideaDatas = IdeaDatas.getIdeaDatas();
         if(ideaDatas.size() <= 0){
             tv_main_idea.setText("출력할 아이디어가 없습니다.\n 전구를 터치해 아이디어를 추가해 주세요!");
             tv_main_idea_date.setText("");
+            tv_main_idea.setOnClickListener(null);
+            tv_main_idea_date.setOnClickListener(null);
             return;
         }
         dto = ideaDatas.getRandomIdea();
         tv_main_idea.setText(dto.getIno_idea());
         tv_main_idea_date.setText(dto.getIno_date());
+
+        tv_main_idea.setOnClickListener(ideaClickEvent);
+        tv_main_idea_date.setOnClickListener(ideaClickEvent);
     }
 
     View.OnClickListener clickedAddIdea = new View.OnClickListener() {
